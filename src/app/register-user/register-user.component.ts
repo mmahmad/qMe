ff16c526ff6e08732b4542740b432046ad66e891
Mohammad Ahmad <mohammadmoaazahmad@gmail.com>
2019-06-09 17:12:28 -0500

    Call service to register user

--- src/app/register-user/register-user.component.ts
+++ src/app/register-user/register-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthServiceService } from '../auth-service.service';
 
 @Component({
   selector: 'app-register-user',
@@ -14,9 +15,17 @@ export class RegisterUserComponent implements OnInit {
     email: '',
     phone_number: ''
   };
-  constructor() { }
+  constructor(private authService: AuthServiceService) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  registerUser() {
+    this.authService.register_user('AbdulRahmen', 'Mustafa', 'test@gmail.com', 'armustafa', '12345', '6179094230')
+    .subscribe(results => {
+      console.log(results);
+    }, err => {
+      console.log(err);
+    });
   }
 
 }
